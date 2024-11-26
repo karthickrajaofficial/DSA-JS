19f3134bafceb453c52d5b7bd405fe4a0cdb7238
karthickrajaofficial <karthickrajaofficial1@gmail.com>
2024-11-26 23:58:27 +0530

    arrays basics

--- 5.Arrays/5.basicsofArray.js
+++ 5.Arrays/5.basicsofArray.js
@@ -15,3 +15,30 @@ class MyArray {
 const myNewArray = new MyArray();
 console.log(myNewArray.push('apple')); // Output: 1
 console.log(myNewArray); // To see the internal state of myNewArray
+
+
+
+//Array declaration 
+
+
+let person ={
+    name:"karthick",
+    age:23
+}
+let arr =["apple","orange","cherry",person]
+console.log(arr.length) //op:4
+//Add remove elements
+
+arr.push("kia") //Adding last 
+console.log(arr) 
+//[ 'apple', 'orange', 'cherry', { name: 'karthick', age: 23 }, 'kia' ]
+
+arr.pop() //remove last
+console.log(arr)
+//[ 'apple', 'orange', 'cherry', { name: 'karthick', age: 23 } ]
+
+arr.unshift("banana") //add 1st
+console.log(arr)
+
+arr.shift() //remove 1st
+console.log(arr)
