7c52e6aaa4ba5249b11c80aec5b0dde6579729e6
karthickrajaofficial <karthickrajaofficial1@gmail.com>
2024-12-18 01:42:19 +0530

    new qns

--- 5.Arrays/14.Anagram.js
+++ 5.Arrays/14.Anagram.js
@@ -9,7 +9,7 @@ const anagram = (s,t) =>{
 console.log(anagram("car","arc"))
 console.log(anagram(123, 321));
 
-
+// ---------------------in array-------------
 const array = ["nuc","arc"]
  const anagram1 = (s,t) => {
    if(s.length !== t.length) return false
@@ -19,3 +19,45 @@ const array = ["nuc","arc"]
 const t = array[1];
  console.log(anagram1(s,t))
  console.log(`The words "${s}" and "${t}" are anagrams: ${anagram1(s, t)}`);
+
+ // ---------------solution2 -----------------
+var isAnagram = function(s, t) {
+  if(s.length !== t.length) return false;
+
+  let obj1 = {}
+  let obj2 = {}
+
+  for(let i = 0; i<s.length ;i++ ){
+      obj1[s[i]] = (obj1[s[i]] || 0) + 1
+      obj2[t[i]] =  (obj2[t[i]] || 0) + 1
+
+  }
+  for (const key in obj1){
+      if(obj1[key] !== obj2[key] ) return false;
+  }
+  return true
+}
+console.log( isAnagram("rat","tar")) //true
+
+// -----------------solution3----------------
+var isAnagram = function(s, t) {
+  if (s.length !== t.length) return false; // If lengths are different, they can't be anagrams
+
+  let obj = {};
+
+  // Count the frequency of characters in string `s`
+  for (let i = 0; i < s.length; i++) {
+      obj[s[i]] = (obj[s[i]] || 0) + 1;
+      obj[t[i]] = (obj[t[i]] || 0) - 1;
+  }
+
+  // If all values are 0, it's an anagram
+  for (const key in obj) {
+      if (obj[key] !== 0) return false;
+  }
+
+  return true;
+};
+
+console.log(isAnagram("rat", "tar")); // true
+console.log(isAnagram("hello", "world")); // false
